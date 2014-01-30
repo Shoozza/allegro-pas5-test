12eb87bada1a0eec1783ba461caef57ddcd2e575
Gregor A. Cieslak <gregor.a.cieslak@gmail.com>
2014-01-31 00:49:13 +0100

    Add event queue with window close event handler instead of waiting 2 seconds

--- test1.pas
+++ test1.pas
@@ -11,6 +11,7 @@ const
 
 var
   Display: ALLEGRO_DISPLAYptr;
+  EventQueue: ALLEGRO_EVENT_QUEUEptr;
 
 procedure init;
 begin
@@ -30,22 +31,46 @@ begin
   end;
 
   al_set_window_title(Display, 'allegro-pas5 test1');
+
+  EventQueue := al_create_event_queue();
+  if EventQueue = nil then
+  begin
+    writeln('create event queue error');
+    halt(1);
+  end;
+
+  al_register_event_source(EventQueue, al_get_display_event_source(display));
 end;
 
 procedure cleanup;
 begin
   WriteLn('cleanup');
 
+  al_destroy_event_queue(EventQueue);
   al_destroy_display(Display);
 end;
 
 procedure run;
+var
+  Running: Boolean;
+  Event: ALLEGRO_EVENT;
 begin
   WriteLn('run');
 
   al_clear_to_color(al_map_rgb(0, 0, 0));
   al_flip_display();
-  al_rest(2);
+
+  Running := true;
+
+  while Running do
+  begin
+    al_wait_for_event(eventQueue, event);
+
+    case event._type of
+      ALLEGRO_EVENT_DISPLAY_CLOSE:
+        running := false;
+    end;
+  end;
 end;
 
 begin

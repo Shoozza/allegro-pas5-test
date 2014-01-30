0fe7ba23ca34129b2946fa4f49c4b2aa4d134e6d
Gregor A. Cieslak <gregor.a.cieslak@gmail.com>
2014-01-30 23:00:37 +0100

    Add init, run, cleanup procedures

--- test1.pas
+++ test1.pas
@@ -2,6 +2,21 @@ program test1;
 
 {$IFNDEF FPC}{$APPTYPE CONSOLE}{$ENDIF}
 
+procedure init;
 begin
+end;
+
+procedure cleanup;
+begin
+end;
+
+procedure run;
+begin
+end;
+
+begin
+  init;
+  run;
+  cleanup;
 end.
 
